563f765b5c08f410a99fd43319de6800479b112f
alifuatakdag <alifuat.akdag@gmail.com>
2023-02-22 07:50:46 +0300

    13.ders

--- src/Sorular_forloops/S14_NestedForLoop.java
+++ src/Sorular_forloops/S14_NestedForLoop.java
@@ -28,7 +28,10 @@ public class S14_NestedForLoop {
         for (int i = 5; i >= 1; i--) {
             for (int j = i; j < 5; j++) {
                 System.out.print("a");
+                j++;
+
             }
+
             for (int k = 1; k <=i ; k++) {
                 System.out.print("* ");
 

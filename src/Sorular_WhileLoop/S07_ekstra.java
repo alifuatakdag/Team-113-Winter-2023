c66988775f42328626aade693fe2ae8841c145c6
alifuatakdag <alifuat.akdag@gmail.com>
2023-02-23 15:23:32 +0300

    15.ders

---
+++ src/Sorular_WhileLoop/S07_ekstra.java
@@ -0,0 +1,19 @@
+package Sorular_WhileLoop;
+
+public class S07_ekstra {
+    public static void main(String[] args) {
+
+        ///*
+        //		 Soru 4) Kullanicidan baslangic ve bitis haflerini alin ve
+        //		 baslangic harfinden baslayip bitis harfinde biten tum harfleri
+        //		 buyuk harf olarak ekrana yazdirin.
+        //		 Kullanicinin hata yapmadigini farz edin.
+        //		 */
+
+
+
+
+
+
+    }
+}

619214cf73ec845b801036a0f0764ea4b81e0de1
alifuatakdag <alifuat.akdag@gmail.com>
2023-02-27 14:27:42 +0300

    17. ders

---
+++ src/grupCalismalari/BoyKiloIndexMethodu.java
@@ -0,0 +1,22 @@
+package grupCalismalari;
+
+public class BoyKiloIndexMethodu {
+
+    public static String kilomNasil(double kilo, double boy) {
+
+        double kitleIndeksi = (kilo) / ((boy / 100) * (boy / 100));
+
+        String boyKiloIndeksSonucu;
+
+        if (kitleIndeksi < 18.5) {
+            boyKiloIndeksSonucu = "Zayif.";
+        } else if (kitleIndeksi >= 18.5 && kitleIndeksi <= 25) {
+            boyKiloIndeksSonucu = "Ideal Kilo";
+        } else if (kitleIndeksi > 25 && kitleIndeksi < 30) {
+            boyKiloIndeksSonucu = "Sisman";
+        } else {
+            boyKiloIndeksSonucu = "Obez";
+        }
+        return boyKiloIndeksSonucu;
+    }
+}
